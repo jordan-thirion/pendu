96bdc620944a84585b041d81f91c8e91dd009cdf
Jordan Thirion <djodjoxyz@gmail.com>
2021-05-07 10:54:58 +0200

    ajout de la bdd simple

---
+++ app.js
@@ -0,0 +1,9 @@
+const MockWords = [
+    { word: "cornedrue", hide: "#o######e" },
+    { word: "cognards", hide: "c######s" },
+    { word: "fourchelang", hide: "########a#g" },
+    { word: "gringotts", hide: "#######tts" },
+    { word: "hyppogriffes", hide: "####o######s" },
+];
+
+db.pendu.insertMany(MockWords)
