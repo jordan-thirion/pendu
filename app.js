ceb5909cec7320d72c9fc71ec58a73eeee6f706d
Jordan Thirion <djodjoxyz@gmail.com>
2021-05-07 15:11:56 +0200

    gestion de l'input de l'user et de la transformation du mot hidden

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ process.stdin.setEncoding('utf8');
 
 //récupérer la valeur de listWordLength pour faire un random number et récuperer le mot de cette index en tant que mot à trouver (done)
 //demander à l'user si il veut jouer (done)
-//gérer l'entré d'un caractere par le user
-//gérer la comparaison avec avec le mot plein
-//gérer l'affichage du mot hidden
+//gérer l'entré d'un caractere par le user (done)
+//gérer la comparaison avec avec le mot plein (done)
+//gérer si il y a plusieurs fois la lettre (done)
+//gérer l'affichage du mot hidden (done)
 //afficher les lettres déja trouvé et deja utilisé
-//update les lettres du mot hidden
+//update les lettres du mot hidden (done)
 //gérer le compteur
 //réussite ou échec
 
@@ -38,22 +39,45 @@ async function game () {
         //entré dans le jeu
         process.stdin.on("data", async (data) => {
             try {
-              const type = data.toString().trim();
-              if (data.toString().trim() === 'y') {
+              const letter = data.toString().trim();
+              if (data.toString().trim() === 'yes') {
                 console.log("So you dare to challenge me? all right!")
-                //console.log(chosenWord)
 
-            } else if (data.toString().trim() === 'n') {
-                console.log('tente ta chance la prochaine fois alors');
+                console.log("My word is... as i would tell you. Instead i'll give you some letters : ")
+                console.log(chosenWord.hide)
+                console.log("now give me a letter")
+                process.stdout.write("choose your letter: > ");
+
+            } else if (data.toString().trim() === 'no') {
+                console.log('Try next time if you dare so :p');
                 process.stdin.pause();
+            } else if (data.toString().trim().length > 1){
+                console.log('Only give one letter please, you cheater')
+                process.stdout.write("choose your letter: > ");
             } else {
-                console.log('You have to say \'y\' or \'n\' for my little processor to understand \n so do you wanna play?')
-                process.stdout.write("y/n? > ");
+                var splitWord = chosenWord.word.split("")
+                var letterExist = splitWord.includes(data.toString().trim())
+                if(letterExist){
+                    var splitWordHidden = chosenWord.hide.split("");
+                    console.log("Congrats you found a letter");
+                    for(var i = 0; i < splitWord.length; i++){
+                        if (splitWord[i] == letter){
+                            splitWordHidden[i] = letter;
+                        }
+                    }
+                    chosenWord.hide = splitWordHidden.join('');
+                    console.log(chosenWord.hide);
+                } else{
+                    console.log("That's not one of my letter...")
+                }
+
+                process.stdout.write("choose your letter: > ");
             }
             } catch (e) {
               await client.close();
               process.stdin.pause();
               console.log(e.message);
+              
             }
           });
 
@@ -68,6 +92,6 @@ async function game () {
 }
 
 console.log("Welcome in the best Hangman game ever done (not sure about it, not gonna lie). \n I'll choose my number and you'll have to guess it. Pretty simple, nah? \n ready to start?")
-process.stdout.write("y/n? > ");
+process.stdout.write("yes/no? > ");
 
 game().catch(console.dir)
