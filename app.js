c0b12702959bcfd9ff1d22e2d4bc82a65034484d
Jordan Thirion <djodjoxyz@gmail.com>
2021-05-07 12:35:01 +0200

    dialogue pour jouer avec l'user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { ExceptionConsole } = require("./src/utils");
 process.stdin.setEncoding('utf8');
 
 //récupérer la valeur de listWordLength pour faire un random number et récuperer le mot de cette index en tant que mot à trouver (done)
+//demander à l'user si il veut jouer (done)
 //gérer l'entré d'un caractere par le user
 //gérer la comparaison avec avec le mot plein
 //gérer l'affichage du mot hidden
@@ -11,6 +12,12 @@ process.stdin.setEncoding('utf8');
 //update les lettres du mot hidden
 //gérer le compteur
 //réussite ou échec
+
+const randomNumber = (max) =>{
+    return Math.floor(
+        Math.random() * (max) + 1
+  )
+}
 
 async function game () {
     try{
@@ -22,19 +29,33 @@ async function game () {
 
         const listWordsLength = await pendu.find({}).count()
 
-        const randomNumber = (max) =>{
-            return Math.floor(
-                Math.random() * (max) + 1
-          )
-        }
-
         const index = randomNumber(listWordsLength);
 
         const query = {"index": index};
 
         const chosenWord = await pendu.findOne(query, proj)
 
-        console.log(chosenWord)
+        //entré dans le jeu
+        process.stdin.on("data", async (data) => {
+            try {
+              const type = data.toString().trim();
+              if (data.toString().trim() === 'y') {
+                console.log("So you dare to challenge me? all right!")
+                //console.log(chosenWord)
+
+            } else if (data.toString().trim() === 'n') {
+                console.log('tente ta chance la prochaine fois alors');
+                process.stdin.pause();
+            } else {
+                console.log('You have to say \'y\' or \'n\' for my little processor to understand \n so do you wanna play?')
+                process.stdout.write("y/n? > ");
+            }
+            } catch (e) {
+              await client.close();
+              process.stdin.pause();
+              console.log(e.message);
+            }
+          });
 
     } catch(e){
         console.log("pb de connexion", e.message);
@@ -45,5 +66,8 @@ async function game () {
     
 
 }
+
+console.log("Welcome in the best Hangman game ever done (not sure about it, not gonna lie). \n I'll choose my number and you'll have to guess it. Pretty simple, nah? \n ready to start?")
+process.stdout.write("y/n? > ");
 
 game().catch(console.dir)
